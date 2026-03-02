e419b8d5f8f67e9b1ed2f5d6004e8658213b851d
gaurav9j0428-droid <gaurav9j0428@gmail.com>
2026-03-03 00:24:35 +0530

    Update server.js

--- REDBUS-CLONE-MEAN-main/backend/server.js
+++ REDBUS-CLONE-MEAN-main/backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use("/review", reviewRoutes);
 
 // MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/tedbus")
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 

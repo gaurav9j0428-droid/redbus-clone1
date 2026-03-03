19434dcde2342d9e2238b473ee5ea9d2d5b5018f
Gaurav Jadhav <gaurav@gmail.com>
2026-03-03 10:41:29 +0530

    render port fix

--- REDBUS-CLONE-MEAN-main/backend/server.js
+++ REDBUS-CLONE-MEAN-main/backend/server.js
@@ -17,7 +17,9 @@ mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-// start server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+// start server (Render compatible)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
